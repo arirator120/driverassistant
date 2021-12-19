902f73966d35e60b69d4bb6190ec9027ecf2f93a
dkaitoo <nguyenduy01645@gmail.com>
2021-12-19 16:01:04 +0700

    Thêm Help, edit info KH

--- DTHDriverAssistant/app/src/main/java/com/example/dthdriverassistant/EditInfoActivity.java
+++ DTHDriverAssistant/app/src/main/java/com/example/dthdriverassistant/EditInfoActivity.java
@@ -3,12 +3,30 @@ package com.example.dthdriverassistant;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class EditInfoActivity extends AppCompatActivity {
 
+    EditText etEditName, etEditPhone;
+    Button btnEditSave, btnEditClose;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_info);
+        init();
+        btnEditClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+    public void init(){
+        etEditName= findViewById(R.id.etEditName);
+        etEditPhone= findViewById(R.id.etEditPhone);
+        btnEditSave= findViewById(R.id.btnEditSave);
+        btnEditClose= findViewById(R.id.btnEditClose);
     }
 }
